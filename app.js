6e3d91855bcce171c96ab019c91dcf6ffae641a6
amirroohi <amirroohi@yahoo.com>
2021-09-14 00:08:03 +0430

    first edition added

---
+++ app.js
@@ -0,0 +1,6 @@
+const groupInput = document.querySelector("#new-product-group");
+const groupButton = document.querySelector("#add-group-btn");
+
+groupButton.addEventListener("click", (e) => {
+    console.log(e.value);
+})
