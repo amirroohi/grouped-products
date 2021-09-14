96a741ded4753ae125dc4628b3149eff872c981f
amirroohi <amirroohi@yahoo.com>
2021-09-14 23:50:56 +0430

    console warning fixed

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const groupInput = document.querySelector("#new-product-group");
 const groupButton = document.querySelector("#add-group-btn");
+const productInput = document.querySelector("#add-new-product")
+let groupNames = [];
+let productNames = [];
+groupInput.addEventListener("input",(e)=>{
+    console.log(e.target.value);
+});
+groupButton.addEventListener("click", () => {
+    let groupInputContext = groupInput.value;
 
-groupButton.addEventListener("click", (e) => {
-    console.log(e.value);
-})
+    groupNames.push(groupInputContext);
+    console.log(groupNames);
+    
+});
