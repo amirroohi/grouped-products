df0cf83e20c95cf304b073d6feffc37c40200695
amirroohi <amirroohi@yahoo.com>
2021-09-15 21:25:27 +0430

    groups and products added to arrays

--- app.js
+++ app.js
@@ -1,15 +1,67 @@
 const groupInput = document.querySelector("#new-product-group");
 const groupButton = document.querySelector("#add-group-btn");
-const productInput = document.querySelector("#add-new-product")
+const productInput = document.querySelector("#add-new-product");
+const groupSelection = document.querySelector("#existed-groups");
+const productButton = document.querySelector("#add-product-btn");
+const productNumber = document.querySelector("#product-number");
+const boardProducts = document.querySelector(".products");
+
+// console.log(groupSelection.value);
 let groupNames = [];
 let productNames = [];
-groupInput.addEventListener("input",(e)=>{
-    console.log(e.target.value);
-});
+// groupInput.addEventListener("input",(e)=>{
+//     console.log(e.target.value);
+// });
+
 groupButton.addEventListener("click", () => {
     let groupInputContext = groupInput.value;
-
-    groupNames.push(groupInputContext);
+    groupInput.value = "";
+    groupNames.push(createGroup(groupInputContext,(groupNames.length+1)));
     console.log(groupNames);
-    
+    // console.log(productNames);
+    addGroupSelection(groupNames);
 });
+productButton.addEventListener("click",() => {
+    let productInputContext = productInput.value;
+    productInput.value = "";
+    productNames.push(createProduct(productInputContext,(productNames.length+1),productNumber.value,groupSelection.value));
+    console.log(productNames);
+    renderProducts(productNames);
+});
+function renderProducts(products){
+    let renderedProducts = "";
+    products.forEach((product) => {
+        renderedProducts += `
+        <div class="product">
+            <p>name:    ${product.productName}</p>
+            <p>quantity:${product.quantity}</p>
+            <p>group:   ${product.group}</p>
+            <p>id:      ${(product.id)}</p>
+        </div>`;
+    });
+    boardProducts.innerHTML = renderedProducts;
+}
+function addGroupSelection(groups){
+    let selection = "";
+    groups.forEach((group) => {
+        selection += `<option>${group.groupName}</option>`;
+    });
+    groupSelection.innerHTML = selection;
+}
+
+// factory
+function createGroup(groupName,id){
+    return {
+        groupName,
+        id,
+    }
+}
+
+function createProduct(productName,id,quantity,group){
+    return {
+        productName,
+        id,
+        quantity,
+        group,
+    }
+}
