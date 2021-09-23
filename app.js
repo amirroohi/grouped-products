4f96ad03603a10e880f4ad6d95012106f6f82399
amirroohi <amirroohi@yahoo.com>
2021-09-23 16:38:41 +0330

    corrected local product is added

--- app.js
+++ app.js
@@ -6,16 +6,12 @@ const productButton = document.querySelector("#add-product-btn");
 const productNumber = document.querySelector("#product-number");
 const boardProducts = document.querySelector(".products");
 const navbarListIcon = document.querySelectorAll(".nav-list-link")
-const deleteProduct = document.querySelector(".delete-btn");
+let deleteProduct = document.querySelectorAll(".delete-btn");
 
 
-// console.log(groupSelection);
 let groupNames = [];
 let productNames = [];
-let deleteBtn = [];
-// groupInput.addEventListener("input",(e)=>{
-    //     console.log(e.target.value);
-    // });
+
     // add events----------------------------
 document.addEventListener("DOMContentLoaded", getGroupSelection);
 document.addEventListener("DOMContentLoaded", getProductRendered);
@@ -24,7 +20,7 @@ groupButton.addEventListener("click", () => {
     let groupInputContext = groupInput.value;
     groupNames.push(createGroup(groupInputContext,(groupNames.length+1)));
     console.log(groupNames);
-    // console.log(productNames);
+    console.log(productNames);
     addGroupSelection(groupNames);
     saveGroupSelection(groupInput.value);
     groupInput.value = "";
@@ -34,8 +30,6 @@ productButton.addEventListener("click",() => {
     let productInputContext = productInput.value;
     let productInformation = createProduct(productInputContext,(productNames.length+1),productNumber.value,groupSelection.value);
     productNames.push(productInformation);
-    console.log(productNames);
-    savedProductQuantity(productNumber.value);
     renderProducts(productNames);
     saveProductRendered(productInformation);
     getProductRendered();
@@ -43,41 +37,28 @@ productButton.addEventListener("click",() => {
     productInput.value = "";
 });
 
-deleteProduct.addEventListener("click",() => {
-    console.log("read");
+boardProducts.addEventListener("click",removeProduct);
 
-});
-// function removeProduct(){
-//     const removeButton = [...document.querySelectorAll(".delete-btn")];
-//     deleteBtn = removeButton;
-//     removeButton.forEach((btn) => {
-//         btn.addEventListener("click",(e) =>{
-//         e.target.innerText = "Deleted";
-//         e.target.disabled = true;
-//         });
-//     });
-// }
-// deleteProduct.addEventListener("click",()=>{});
-// function deletedProduct(){
-//     productNames.splice((productNames.id)-1,1);
-// }
-// show products on board----------------
 function renderProducts(products){
     let renderedProducts = "";
     products.forEach((product) => {
         renderedProducts += `
-        <div class="product">
+        <div class="product" >
             <p><span>${product.group}</span>  has
             <span>${product.quantity}</span> number of
             <span>${product.productName}</span>  product(s).
             </p>
-            <button type="button" class="delete-btn">Delete</button>
+            <button type="button" class="delete-btn" data-id="${product.id}">Delete</button>
         </div>`;
     });
     boardProducts.innerHTML = renderedProducts;
 }
 function removeProduct(deleteBtn){
-    console.log(deleteBtn);
+    if(deleteBtn.target.textContent == "Delete"){
+        const parentDeleteBtn = deleteBtn.target.parentElement;
+        updatelocalproducts(parentDeleteBtn);
+        parentDeleteBtn.remove();
+    }
 }
 // show group in select tag
 function addGroupSelection(groups){
@@ -108,9 +89,7 @@ function createProduct(productName,id,quantity,group){
 // groups
 function saveGroupSelection(group){
     let savedGroups = localStorage.getItem("groups") ? JSON.parse(localStorage.getItem("groups")) : [];
-    // if(group !== ""){
     savedGroups.push(group);
-    // }
     localStorage.setItem("groups", JSON.stringify(savedGroups));
 }
 
@@ -118,9 +97,7 @@ function getGroupSelection(){
     let savedGroups = localStorage.getItem("groups") ? JSON.parse(localStorage.getItem("groups")) : [];
     let selection = "";
     savedGroups.forEach((groups) => {
-        // if(groups !== ""){
         selection += `<option>${groups}</option>`;
-        // }
     groupSelection.innerHTML = selection;
     });
 }
@@ -129,6 +106,7 @@ function getGroupSelection(){
 function saveProductRendered(product){
     let savedProducts = localStorage.getItem("products") ? JSON.parse(localStorage.getItem("products")) : [];
     savedProducts.push(product);
+    productNames = savedProducts;
     localStorage.setItem("products", JSON.stringify(savedProducts));
 }
 function getProductRendered(){
@@ -136,16 +114,19 @@ function getProductRendered(){
     let renderedProducts = "";
         savedProducts.forEach((product) => { 
                 renderedProducts += `
-                <div class="product">
-                    <p><span>${product.group}</span>  has
-                    <span>${product.quantity}</span> number of
-                    <span>${product.productName}</span>  product(s).
-                    </p>
-                    <button type="button" class="delete-btn">Delete</button>
+                <div class="product" >
+                    <p><span>${product.group}</span> has <span>${product.quantity}</span> number of <span>${product.productName}</span> product(s).</p>
+                    <button type="button" class="delete-btn" data-id="${product.id}">Delete</button>
                 </div>`;
             });
 
     boardProducts.innerHTML = renderedProducts;
 }
-// quantity
-
+// remove product
+function updatelocalproducts(product){
+    const savedProducts = localStorage.getItem("products") ? JSON.parse(localStorage.getItem("products")) : [];
+    let productText = product.children[0].textContent;
+    let productTextArray = productText.split(" ");
+    let existedProducts = savedProducts.filter(p => p.productName !== productTextArray[5]);
+    localStorage.setItem("products",JSON.stringify(existedProducts));
+}
